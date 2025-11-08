5fa6fc5962b6cfc4895340262427da1fe43949b7
Abdulmajid Yunusov <goldendevuz@gmail.com>
2025-11-08 09:35:11 +0000

    2025.11.08/14:35

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import aiohttp
 import asyncio
 import logging
 import sys
@@ -10,52 +9,32 @@ from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-import requests
+import wikipedia
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Bot token can be obtained via https://t.me/BotFather
-TOKEN = getenv("BOT_TOKEN", "your bot token")
-API_KEY = getenv("API_KEY", "your weather token")
+TOKEN = getenv("BOT_TOKEN")
+
+wikipedia.set_lang('uz')
 
 # All handlers should be attached to the Router (or Dispatcher)
 
 dp = Dispatcher()
 
-async def get_temp(city):
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric") as response:
-            data = await response.json()
-
-            if data.get("cod") == 200:
-                return data["main"].get("temp")
-
-
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
-
+    await message.reply("Wikipeida Botiga Xush Kelibsiz!")
 
 @dp.message()
-async def weather_handler(message: Message) -> None:
+async def send_wiki(message: Message) -> None:
     try:
-        temp = await get_temp(message.text)
-        if temp:
-            await message.reply(f"{temp}℃")
-        else:
-            await message.reply("City not found")
-
-    except TypeError:
-        await message.reply("Exception was occured")
-    
-    return 
-
+        respond = wikipedia.summary(message.text)
+        await message.reply(respond)
+    except:
+        await message.reply("Bu mavzuga oid maqola topilmadi")
 
 async def main() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
